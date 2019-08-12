297ef71dce59230bf9bfdddf5e5342419ab82e80
arieee <ari.szk61@gmail.com>
2019-08-12 21:50:00 +0900

    fix bug and old config style of tmux

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,6 +3,5 @@ ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.emacs.d ~/.emacs.d
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.gitignore ~/.gitignore
